33df39da252dee4225ab24e5dd370be7617e10f8
makar.eth <makar.ananyin@yandex.ru>
2022-06-15 20:13:30 +0300

    add realization of geckoIdByName and platformById

---
+++ addressByName/addressByName.py
@@ -0,0 +1,35 @@
+from config import *
+import requests as r
+
+def addressByName(name):
+    id = geckoIdByName(name)
+    if len(id):
+        pass
+    else:
+        return 1
+
+def geckoIdByName(name):
+    response = r.get(API_URL + f"/search?query={name}")
+    if response:
+        data = response.json()
+        if len(data['coins']) == 0:
+            ValueError("Could not find coin \"{name}\"")
+        else:
+            return data['coins'][0]['id']
+    else:
+        ValueError(f"Something wrong with response\n{response.status_code}\n{response.text}")
+
+def platformsById(id:str):
+    response = r.get(API_URL + f"/coins/{id}")
+    if response:
+        data = response.json()
+        try:
+            return data["platforms"]
+        except:
+            ValueError(f"Could not find coin with the given id: {id}")
+    else:
+        ValueError(f"Something wrong with response\n{response.status_code}\n{response.text}")
+
+if __name__ == '__main__':
+    print(geckoIdByName("ethereum"))
+    print(platformsById("pancakeswap-token"))
--- addressByName/config.py
+++ addressByName/config.py
@@ -1,1 +1,1 @@
-GeckoAPIKey = ""
+API_URL = "https://api.coingecko.com/api/v3"
